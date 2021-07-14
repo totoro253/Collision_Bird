059de65fa3e74e8b72a01c73ac2d80d12b354fbf
Fsetrodinomo <delicateoutlier@gmail.com>
2021-07-14 03:28:42 +0200

    exploring data
    so far no NA's, interest in flightcall-species ,  try to find a way to visualize in a way which helps me to understand the data

--- Data/Processed/Cleaning.R
+++ Data/Processed/Cleaning.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(psych)
 library(jmv)
 library(heatmaply)
+library(inspectdf)
+library(tibble)
 
 bird_collisions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/bird_collisions.csv")
 mp_light <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-30/mp_light.csv")
@@ -11,22 +13,25 @@ mp_light <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/t
 bird_collisions <- bird_collisions %>% janitor::clean_names()
 mp_light<- mp_light %>% janitor::clean_names()
 
+summary(bird_collisions)
+summary(mp_light)
+
+bird_collisions %>% inspectdf::inspect_na()
+mp_light %>% inspectdf::inspect_na()
+
+bird_collisions %>% inspectdf::inspect_cat()
+
 names(bird_collisions)
 names(mp_light)
 
-genus <- table (bird_collisions$genus)
-species <-table (bird_collisions$species)
-dates <- table (bird_collisions$date)
-locality <- table (bird_collisions$locality)
-familyb <- table (bird_collisions$family)
-flight_call <- table ( bird_collisions$flight_call)
-habitat <-table  (bird_collisions$habitat)
-stratum <- table (bird_collisions$stratum)
-
-barplot(genus)
-barplot(species)
-barplot()
-
+genus <-  (bird_collisions$genus)
+species <- (bird_collisions$species)
+dates <-  (bird_collisions$date)
+locality <-  (bird_collisions$locality)
+familyb <-  (bird_collisions$family)
+flight_call <-  ( bird_collisions$flight_call)
+habitat <-  (bird_collisions$habitat)
+stratum <-  (bird_collisions$stratum)
 
 describe(bird_collisions[1:8])
 describe(mp_light[1:2])
@@ -38,7 +43,21 @@ bird_collisions %>% count(bird_collisions$flight_call)  #overview count birds do
 bird_collisions %>% count(bird_collisions$genus)
 bird_collisions %>% count(bird_collisions$species)
 
+
 skim(bird_collisions)
 colSums(is.na(bird_collisions))  #checking NA'S
 
-df_flightSpecies <- data.frame(flight_call,species)
+#Use table to  barplot
+t_stratum <- table  (bird_collisions$stratum)
+t_speciesFlight <- table (bird_collisions$species,bird_collisions$flight_call)
+
+
+
+df_speciesFlight <- data.frame(species,flight_call)
+cat_speciesFlight <- df_speciesFlight %>% inspectdf::inspect_cat()
+cat_speciesFlight$levels$Var1
+
+#Exploring which species using flight calls
+t_speciesFlight <- table (bird_collisions$species,bird_collisions$flight_call)
+barplot(t_speciesFlight)
+
