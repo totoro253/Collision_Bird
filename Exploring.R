814b7aecea14e81010cd91a091d52efe400996a7
Totoro253 <61892539+totoro253@users.noreply.github.com>
2023-09-17 19:38:25 +0200

    plot species based on locality

--- Exploring.R
+++ Exploring.R
@@ -18,3 +18,13 @@ ggplot(bird_collisions,aes(bird_collisions$locality,fill=bird_collisions$species
   labs(x="Locality",
        y="Species",
        title="Species based on locality")
+
+ggplot(bird_collisions,aes(x=bird_collisions$date))+
+  geom_density(aes(fill=factor(bird_collisions$flight_call)),alpha=0.5)+
+  labs(title="Flight call based on date",
+       x="date",
+       fill="flightcall")
+
+
+ggplot(bird_collisions,aes(bird_collisions$date,y=bird_collisions$habitat,color=bird_collisions$species))+
+  geom_point()+facet_wrap(~bird_collisions$flight_call)
